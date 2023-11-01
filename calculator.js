12e59ff3225d1ae3b636533f9f368734cc58421e
Raí <143852565+Rai9s@users.noreply.github.com>
2023-11-01 07:36:41 -0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = displayValue;
+}
